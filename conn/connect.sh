504b032493a306c4ed6819cc3e8812e824a237dd
Brendan O'Connor <brendan@maliceafterthought.com>
2012-09-22 20:46:25 -0500

    Adding connect and scan scripts, now with auto connection failure detection.

---
+++ conn/connect.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+PREFIX=/home/pi/piglet/conn
+
+while [ 1 ]
+do
+pkill dhclient
+pkill wpa_supplicant #So I can run it again
+
+sleep 2 # Without a bit of sleep, this thing freaks out from the pkilling.
+
+ifconfig wlan0 up
+#ifconfig wlan1 up
+perl $PREFIX/scantosupp.pl wlan0 $PREFIX/out.conf
+wpa_supplicant -B -i wlan0 -c $PREFIX/out.conf
+
+# OK, so now it's running.
+
+numnetworks=`wpa_cli list_networks | wc -l` # Gives us the number of networks, +2, that wpa_cli sees
+numnetworks=`expr $numnetworks - 2`
+echo "$numnetworks networks found that we may (or may not) have the credentials to connect to."
+
+success=0
+
+for ((a=0; a < numnetworks ; a++))
+do
+	echo "Now connecting to network $a."
+	wpa_cli select $a
+	sleep 2 #It seems to take a second to do the switch.
+	wpa_cli status
+	dhclient wlan0 -1 #timeout of 20 seconds in /etc/dhcp/dhclient.conf
+	if [ $? -eq 0 ] #Yay, it worked!
+		then ping -c 3 -I wlan0 8.8.8.8
+		if [ $? -eq 0 ] #Houston, we have liftoff.
+			then echo "Success!"
+			success=1
+			break
+		fi
+	fi
+	echo "No success with network $a."
+done
+
+if [ $success ]
+	then
+	while ping -c 3 -I wlan0 8.8.8.8
+	do
+		echo "`date` Connection alive. Sleeping for 5 seconds."
+		sleep 5
+	done
+else
+	echo "No success. Sleeping for five seconds."
+	sleep 5
+fi
+
+echo "Connection has failed; restarting connection attempt."
+
+pkill dhclient
+pkill wpa_supplicant
+rm $PREFIX/out.conf
+
+done
+
+
+
---
+++ conn/scantosupp.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+my $device = shift;
+my $outfile = shift;
+
+use YAML::Syck;
+
+my $knownnetworks = LoadFile("networks.yaml");
+
+unless ($device && $outfile)
+{
+    print "Usage: scantosupp.pl <devicename> <output file>";
+    exit(1);
+}
+
+open(FILE, ">$outfile");
+
+my $networklist = `iwlist $device scan`;
+
+my @networks = split(/Cell \d{2}/, $networklist); #This will give us cell 1 in @networks[1], as [0] will hold junk
+
+delete $networks[0];
+
+print FILE "ctrl_interface=DIR=/var/run/wpa_supplicant\n";
+
+foreach (@networks)
+{
+    my $net = $_;
+    my @data = split(/\n/, $net);
+    my $bssid = $data[0];
+    $bssid =~ /([A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2}:[A-F0-9]{2})/;
+    $bssid = $1;
+    
+    my $ssid = $data[5];
+    $ssid =~ /ESSID\:\"(.*)\"/;
+    $ssid = $1;
+    
+    my $enc = $data[4];
+    $enc = /Encryption key:(.+)/;
+    $enc = $1;
+   
+ 
+	# TODO FIX: This doesn't actually grab full open networks, fix the logic here
+	
+	if ($knownnetworks->{$ssid}) # Don't bother with networks whose keys we don't know.
+	{
+	    print FILE "network={\n";
+	    print FILE "ssid=\"$ssid\"\n";
+	    print FILE "scan_ssid=1\n";
+	    if ($enc eq "on")
+	    {
+	        # Also this is just a brutal hack. I can be a lot more precise-- specifying CCMP and the like-- but it doesn't matter, weirdly.
+	        if ($net =~ /WPA/) #Because it doesn't seem to matter WPA/WPA2, which is concerning....
+	        {
+	            print FILE "key_mgmt=WPA-PSK\n";
+	            print FILE "psk=\"$knownnetworks->{$ssid}\"\n";
+	        }
+	        else # Then it's WEP
+	        {
+	            #For WEP, we're going to assume KEY ID 0, because almost no one ever uses any other key index.
+	            print FILE "key_mgmt=NONE\n";
+				print FILE "wep_tx_keyidx=0\n";
+                print FILE "wep_key0=$knownnetworks->{$ssid}\n";			
+	        }
+	    }
+	    else
+	    {
+	        print FILE "key_mgmt=NONE\n";
+	    }
+    
+	    print FILE "}\n\n";
+	}
+}
+
+
+close(FILE);
+
